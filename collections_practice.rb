9404eba532c7c691dd7bc306e4caf1981b16d912
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 17:52:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,7 +5,12 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  array.sort do |a, b|
+  array.sort! do |a, b|
     b <=> a
   end
+end
+
+def sort_array_char_count(array)
+  array.sort
+
 end
