a4bab98c2d537add4a344ca93962607b24ec1137
Daniel Reina <dannyflatiron@gmail.com>
2019-11-11 18:05:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,12 @@ def sort_array_char_count(array)
   array.sort_by {|d| d.length}
 end
 
+# def swap_elements(array)
+#   # array[0], array[1], array[2] = array[0], array[2], array[1]
+#   array[1], array[2] = array[2], array[1]
+# end
+
 def swap_elements(array)
-  # array[0], array[1], array[2] = array[0], array[2], array[1]
   array[1], array[2] = array[2], array[1]
+  array
 end
